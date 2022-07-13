2d2e9bbc209b1e1ea44069efe2edfd1f6600dd69
gperezmal <e_gpmalca@latam.stefanini.com>
2022-07-13 09:36:53 -0500

    .

--- src/main/java/com/example/demo/controller/ActorController.java
+++ src/main/java/com/example/demo/controller/ActorController.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Actor;
 import com.example.demo.service.ActorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,25 +25,25 @@ public class ActorController {
     @Autowired
     ActorService actorService;
 
-    @Operation(summary = "Creación de usuarios", tags = "Actores")
+    @Operation(summary = "Creación de Actores", tags = "Actores")
     @PostMapping("/create")
     public Actor crear(@RequestBody Actor actor) {
         return actorService.add(actor);
     }
 
-    @Operation(summary = "Listar usuario por Id", tags = "Actores")
+    @Operation(summary = "Listar Actore por Id", tags = "Actores")
     @GetMapping("/obtenerId/{id}")
     public Actor getById(@PathVariable int id) {
         return actorService.getById(id);
     }
 
-    @Operation(summary = "Listar usuarios", tags = "Actores")
+    @Operation(summary = "Listar Actores", tags = "Actores")
     @ApiResponse(
             responseCode = "200",
-            description = "Lista de Usuarios")
+            description = "Lista de Actores")
     @ApiResponse(
             responseCode = "404",
-            description = "Usuario no encontrado o Error de BD")
+            description = "Actor no encontrado o Error de BD")
 
     @ApiResponse(
             responseCode = "500",
@@ -55,15 +54,15 @@ public class ActorController {
         return actorService.list();
     }
 
-    @Operation(summary = "Actualizacion de usuario", tags = "Actores")
+    @Operation(summary = "Actualizacion de Actor", tags = "Actores")
     @PutMapping("/updateUser/{id}")
     public Actor updateActor(@PathVariable int id, @RequestBody Actor actorDetails) {
         return actorService.updateActor(id, actorDetails);
     }
 
-    @Operation(summary = "Eliminacion de usuario", tags = "Actores")
+    @Operation(summary = "Eliminacion de Actor", tags = "Actores")
     @DeleteMapping("/deleteUser/{id}")
-    public String deleteUser(@PathVariable int id) {
+    public String deleteActor(@PathVariable int id) {
         return  actorService.deleteActor(id);
     }
 
